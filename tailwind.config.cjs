93bfdd41238a0d241c38eb8c28be93c799e691fb
emil backlund <emil_backi_@hotmail.com>
2022-11-21 19:02:13 +0100

    complete homepage
    start on detailpage next

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/*.{html,js}', './src/js/*.js', './src/js/helpers/*.js'],
+  theme: {
+    extend: {
+      fontFamily: {
+        signika: ['Signika Negative', 'sans-serif'],
+        syne: ['Syne Mono', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+const { resolve } = require('path');
+import { defineConfig } from 'vite';
+
+export default defineConfig ({
+  root: resolve(__dirname, 'src'),
+  publicDir: resolve('public'),
+  preview: {
+    host: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        homePage: resolve(__dirname, 'src/index.html'),
+        profilePage: resolve(__dirname, 'src/profile.html'),
+      },
+    },
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+
+  resolve: {
+    alias: {},
+  },
+  server: {
+    port: 8080,
+    hot: true,
+  },
+});
